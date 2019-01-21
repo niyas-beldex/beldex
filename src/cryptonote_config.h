95edb1defcee32e1afdc6bcccdeee805df6a98c2
Mohammed Niyas <mohammed.niyas@beldex.org>
2019-01-21 09:49:32 +0530

    added genesis transaction hash for testnet

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -182,7 +182,7 @@ namespace config
     boost::uuids::uuid const NETWORK_ID = { {
         0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x14 , 0x41, 0x61, 0x17, 0x31, 0x09, 0x82, 0x26, 0xA1, 0xA1, 0x11
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "";
+    std::string const GENESIS_TX = "013c01ff0003ffaf89d702029d09f4e9ee8f0bd89b851eb3853095ab5035f84076a319f3cc104377ec7fab3780a0d9e61d02d298cd09ba6ae2c2c4e0c85e566e0a6da0166e2d04bf8b3af179dec96aa1a53480b09dc2df01028dafb21a88e689bd122ff4a80caee920b195948043816a012c47069ec85213092101301d1e74d63cec307fda2a5b744be8b32febc0e8d78c97e03ddac5464ac37e6d";
     uint32_t const GENESIS_NONCE = 113366;
   }
 
